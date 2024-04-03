bfe6fbc1b1f047cdf6f2b7404b6457be85cfc3a5
Baris <konencally@gmail.com>
2024-04-03 16:55:05 +0300

    first commit

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fcioefaa:4JSksmDkw2uNLN-vB1Ut-qnzGzw9z-6L@cow.rmq2.cloudamqp.com/fcioefaa");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue",true,false,false);
+
+string message = "hello world";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+Console.WriteLine("Mesaj gönderilmiştir.");
+
+Console.ReadLine();
