58e9f78de27e7ad0b1dad0a3bc18599d2a005895
Baris <konencally@gmail.com>
2024-04-04 18:18:51 +0300

    first commit

--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.Loader;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://fcioefaa:4JSksmDkw2uNLN-vB1Ut-qnzGzw9z-6L@cow.rmq2.cloudamqp.com/fcioefaa");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//burasi istege bagli,publisherın kuyruğu oluşturduğuna emin isek kaldırabiliriz kuyruk yoksa oluşturur
+//channel.QueueDeclare("hello-queue",true,false,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue",true,consumer);
+
+consumer.Received += (object AssemblyDependencyResolver, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen mesaj: " + message);
+};
+
+Console.ReadLine();
