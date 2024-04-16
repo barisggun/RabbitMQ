737f4d49f3b56658b0f4089e65e6360e8eb4e1e5
Baris <konencally@gmail.com>
2024-04-16 11:02:29 +0300

    add unversioned files

---
+++ RabbitMQWeb.Watermark/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RabbitMQWeb.Watermark.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
